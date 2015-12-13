382a814a009cd84e58fa012115060de395771c25
mario_loncar <loncar.mario1@gmail.com>
2015-12-13 02:15:59 +0100

    - splash - screenshots

--- app/src/main/java/com/mario/pharmacy/Splash.java
+++ app/src/main/java/com/mario/pharmacy/Splash.java
@@ -3,5 +3,30 @@ package com.mario.pharmacy;
 /**
  * Created by mario on 12/13/15.
  */
-public class Splash {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    //Duration of splash
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash);
+
+        //Start activity and close it in few seconds
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this, MapsActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
