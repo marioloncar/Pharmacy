41a6a67bab3e94f73c7b783587a4c4975a43574f
mario_loncar <loncar.mario1@gmail.com>
2015-12-20 21:30:31 +0100

    - resized map markers

--- app/src/main/java/com/mario/pharmacy/MapsActivity.java
+++ app/src/main/java/com/mario/pharmacy/MapsActivity.java
@@ -4,11 +4,19 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,6 +35,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     ArrayList<String> latitudeArray, longitudeArray, name, address;
     double latitude, longitude;
     LatLng coordinates;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +53,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         //Make database
         helper = new DatabaseHelper(this);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -55,67 +71,111 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) && !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    //All location services are disabled. Show dialog.
+                    locationAlert();
+                }
+                return false;
+            }
+        });
 
         checkConnectivity();
     }
 
     public void checkConnectivity() {
-        if (isNetworkAvailable()) {
-            // Add a marker in Sydney and move the camera
-            LatLng rijeka = new LatLng(45.329904, 14.438916);
-            latitudeArray = helper.getLatitude();
-            longitudeArray = helper.getLongitude();
-            name = helper.getName();
-            address = helper.getAddress();
-
-            for (int i = 0; i < latitudeArray.size(); i++) {
-                latitude = Double.parseDouble(latitudeArray.get(i));
-                longitude = Double.parseDouble(longitudeArray.get(i));
-                coordinates = new LatLng(latitude, longitude);
-                mMap.addMarker(new MarkerOptions()
-                        .position(coordinates)
-                        .title(name.get(i))
-                        .snippet(address.get(i))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-            }
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(rijeka, 15));
-
-            mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
-                @Override
-                public void onInfoWindowClick(Marker marker) {
-                    Intent intent = new Intent(getApplicationContext(), Info.class);
-                    intent.putExtra("name", marker.getTitle());
-                    startActivity(intent);
-                }
-            });
+        if (!isNetworkAvailable()) {
+            connectionAlert();
         } else {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setCancelable(false);
-            builder.setTitle("No internet connection");
-            builder.setMessage("Please check your internet connection and try again.");
-            builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    checkConnectivity(); //ponovno provjeravanje
-                    dialog.dismiss();
-
-                }
-            });
-            builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
-            builder.setNeutralButton("Settings", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    startActivity(new Intent(android.provider.Settings.ACTION_SETTINGS));
-                }
-            });
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            setUpMap();
         }
+    }
+
+    private void setUpMap() {
+        LatLng rijeka = new LatLng(45.329904, 14.438916);
+        latitudeArray = helper.getLatitude();
+        longitudeArray = helper.getLongitude();
+        name = helper.getName();
+        address = helper.getAddress();
+
+        for (int i = 0; i < latitudeArray.size(); i++) {
+            latitude = Double.parseDouble(latitudeArray.get(i));
+            longitude = Double.parseDouble(longitudeArray.get(i));
+            coordinates = new LatLng(latitude, longitude);
+            mMap.addMarker(new MarkerOptions()
+                    .position(coordinates)
+                    .title(name.get(i))
+                    .snippet(address.get(i) + " (Click for info)")
+                    .icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("marker", 100, 100))));
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(rijeka, 15));
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(getApplicationContext(), Info.class);
+                intent.putExtra("name", marker.getTitle());
+                startActivity(intent);
+            }
+        });
+    }
+
+    public Bitmap resizeMapIcons(String iconName, int width, int height) {
+        Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(), getResources().getIdentifier(iconName, "drawable", getPackageName()));
+        return Bitmap.createScaledBitmap(imageBitmap, width, height, false);
+    }
+
+    private void locationAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+        builder.setTitle("Location services disabled");
+        builder.setMessage("Google Maps needs access to your location. Please turn on location access to get your current location.");
+        builder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        });
+        builder.setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                //builder.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void connectionAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setTitle("No internet connection");
+        builder.setMessage("Please check your internet connection and try again.");
+        builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                checkConnectivity(); //ponovno provjeravanje
+                dialog.dismiss();
+
+            }
+        });
+        builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNeutralButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     private boolean isNetworkAvailable() {
@@ -127,8 +187,42 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.mario.pharmacy/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.mario.pharmacy/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
